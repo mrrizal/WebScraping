e5b48a2a2d7d0984c69d9c9404b85fb5aef8ab37
rizal <rizalubuntuuser@gmail.com>
2016-08-23 17:52:40 +0700

    edit waktuku

--- waktuku.py
+++ waktuku.py
@@ -3,10 +3,26 @@ from bs4 import BeautifulSoup
 import re
 
 def getBreakingNews(url):
+	headers = {
+		"User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:44.0) Gecko/20100101 Firefox/44.0"
+	}
+	values = {}
 	r =  requests.get(url)
 	soup = BeautifulSoup(r.content, "html5lib")
-	datas = soup.find_all('h3', {"class":"entry-title td-module-title"})
+	datas = soup.find_all('div', {"class":"td-trending-now-display-area"})
+	for data in datas:
+		breakingNews = data.find_all('a')
+		for breakingNew in breakingNews:
+			try:
+				values[breakingNew.text] = breakingNew.get('href')
+			except:
+				pass
+
+	return values
+
+	'''
 	values = {}
+	return va
 	for data in datas:
 		try:
 			values[data.text] = data.find_all('a')[0].get('href')
@@ -14,6 +30,7 @@ def getBreakingNews(url):
 			pass
 
 	return values
+	'''
 
 def getContent(url):
 	r = requests.get(url)
@@ -29,7 +46,7 @@ def main():
 	url = 'http://waktuku.com/'
 	data = getBreakingNews(url)
 	keys = list(data.keys())
-
+	
 	while True:
 		counter = 1
 		for key in keys:
@@ -44,7 +61,7 @@ def main():
 		page = data[keys[pilih-1]]
 		results = getContent(page)
 		print("\n",results,"\n")
-
+	
 
 if __name__ == '__main__':
 	main()
